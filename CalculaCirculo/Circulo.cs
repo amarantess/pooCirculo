2853b4e4c8bb60fc753bb1de22fdd699e4d93078
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-04-15 19:08:07 -0300

    primeiro commit

---
+++ CalculaCirculo/Circulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaCirculo
+{
+    public class Circulo
+    {
+        // Propriedade
+        private double raio;
+
+        public double Raio
+        {
+            get { return raio; }
+            set { raio = value; }
+        }
+
+        // Métodos
+        public void CalculaArea()
+        {
+            double calculo = 3.14 * (this.Raio * this.Raio);
+            Console.WriteLine($"A área do círculo é: {calculo}");
+        }
+
+        public void CalculaPerimetro()
+        {
+            double calculoP = 2 * 3.14 * this.Raio;
+            Console.WriteLine($"O Perímetro do círculo é: {calculoP}");
+        }
+    }
+}
---
+++ CalculaCirculo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaCirculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo c = new Circulo();
+
+            Console.WriteLine("##CALCULA ÁREA E PERÍMETRO##");
+            Console.WriteLine();
+
+        Inicio:
+            Console.Clear();
+
+            Console.Write("Informe o raio do círculo: ");
+            c.Raio = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            c.CalculaArea();
+            c.CalculaPerimetro();
+
+            Console.WriteLine();
+            Console.WriteLine("Deseja repetir? Sim/Não");
+            string opcao = Console.ReadLine();
+
+            if (opcao == "Sim" || opcao == "sim") goto Inicio;
+
+
+            Console.ReadKey();
+        }
+    }
+}
